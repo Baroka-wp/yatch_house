47fd971743475ccb1fe0d09ee1df6e79080a59f0
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-19 15:09:19 +0200

    Create house model

---
+++ app/models/house.rb
@@ -0,0 +1,8 @@
+class House < ApplicationRecord
+  has_many :reservations
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+end
