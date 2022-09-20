a4de7b0e86eae36de5027abae8f8b8981eede2da
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-20 16:42:01 +0200

    Create reservations_controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::ReservationsController < ApplicationController
+    def index
+        @reservations=Reservation.all
+        render json: @reservations
+    end
+
+    def show
+        @reservation=Reservation.find(:params)
+        render json: @reservation
+    end
+
+    def new
+    end
+    
+    def create
+        @reservation=current_user.reservations.new(reservation_params)
+        @reservation.save
+    end
+    
+    def destroy
+        @reservation=Reservation.find(:params)
+        @reservation.destroy  
+    end
 end
