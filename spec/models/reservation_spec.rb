2c7dbe2d51a9c4be7eff8731e2518842112a4e87
yasin <yasin.elsharawi23@gmail.com>
2022-09-20 14:03:21 +0300

    Add tests for reservation.rb

---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Reservation, type: :model do
+  describe 'Reservation model' do
+    user1 = User.new(name: 'Max', telephone: 41_234_123, email: 'Max@example.com', password_digest: '123456')
+    house1 = House.new(name: 'Mansion1', location: 'Hollywood', description: 'The description of the house',
+                       price: 8_000_000, image: 'https://unsplash.com/photos/F_-0BxGuVvo')
+    subject do
+      Reservation.new(start_date: '02-01-2008', end_date: '04-09-2022', status: 'open', house_id: house1.id,
+                      user_id: user1.id)
+    end
+    before { subject.save }
+
+    it 'check the start_date is not blank' do
+      subject.start_date = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check the end_date is not blank' do
+      subject.end_date = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check the status is not blank' do
+      subject.status = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
