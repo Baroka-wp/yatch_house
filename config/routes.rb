617af080c8df42d28fbb471dd56a8bda24ee8cd9
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-21 19:56:36 +0200

    Add sessions and registration controllers path to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
