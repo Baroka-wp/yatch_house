75879935561bf202b322cb8bebe449f7f125f634
yasin <yasin.elsharawi23@gmail.com>
2022-09-22 15:50:30 +0300

    Update tests for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'User model' do
-    subject { User.new(name: 'Yasin', telephone: 41_234_123, email: 'ogaga@example.com', password_digest: '123456') }
+    subject { User.new(name: 'Yasin', telephone: 41_234_123, email: 'ogaga@example.com', password: '123456') }
     before { subject.save }
 
     it 'check the name is not blank' do
