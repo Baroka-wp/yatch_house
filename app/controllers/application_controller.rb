42dc1e6022c74de318764650d7cbe26938fb3e15
baroka <birotori@gmail.com>
2022-09-23 00:15:47 +0100

    Add permited parameters to applicaiton controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::API
   protected
  
   def configure_permitted_parameters
-     devise_parameter_sanitizer.for(:sign_up).push(:name,:email, :telephone, :admin, :password, :password_confirmation)
-     devise_parameter_sanitizer.for(:account_update).push(:name,:email, :telephone, :admin, :password)
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name,:email, :telephone, :admin, :password, :password_confirmation) }
   end
 end
