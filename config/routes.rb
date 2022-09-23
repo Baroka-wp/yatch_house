6663ddcc6b5bd7b22a352f7c65d429acf7359a88
GedeonTS <gedeontshobohwa1@gmail.com>
2022-09-23 16:20:45 +0200

    Refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,6 @@ Rails.application.routes.draw do
       sessions: 'users/sessions',
       registrations: 'users/registrations'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
