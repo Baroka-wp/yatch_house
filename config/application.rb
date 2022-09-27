c2bdf1af408bff4c76faf9d3f00a3b8d56e8d8cf
baroka <birotori@gmail.com>
2022-09-27 17:10:40 +0100

    Change application to add sprockets/railtie

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,8 @@
 require_relative "boot"
 
-# require "rails/all"
+require "rails/all"
 require 'sprockets/railtie'
-require "active_storage/engine"
+# require "active_storage/engine"
 
 
 # Require the gems listed in Gemfile, including any gems
