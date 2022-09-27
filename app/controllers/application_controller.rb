e518a459bbb7e1bbe8cfa04bc92bb1866dfca492
baroka <birotori@gmail.com>
2022-09-27 20:28:55 +0100

    Run spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
