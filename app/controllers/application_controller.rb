fdee44499308d6c9b3acc4f03026677c543d8045
baroka <birotori@gmail.com>
2022-09-28 00:50:33 +0100

    Disable rake task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
